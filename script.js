ccae0329bc0ff621dc6b13f3fdfb70b37c74740f
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-15 16:17:29 +0200

    add card

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 
 // ^RACCOLTA DATI
 
+const info = document.getElementById("text-info");
+const card = document.getElementById("card");
+
+// console.log(info);
+// console.log(cardMembers);
+
 const teamMembers = [
   {
     name: "Marco Bianchi",
@@ -40,3 +46,122 @@ const teamMembers = [
     img: "img/female3.png",
   },
 ];
+
+// ^SVOLGIMENTO
+
+const cardMembers = () => {
+  card.innerHTML = `
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="./${img}" class="img-fluid rounded-start" alt="...">
+          </div>
+
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${role}</p>
+              <p class="card-text"><small class="text-body-secondary">${email}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="./${img}" class="img-fluid rounded-start" alt="...">
+          </div>
+
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${role}</p>
+              <p class="card-text"><small class="text-body-secondary">${email}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="./${img}" class="img-fluid rounded-start" alt="...">
+          </div>
+
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${role}</p>
+              <p class="card-text"><small class="text-body-secondary">${email}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="./${img}" class="img-fluid rounded-start" alt="...">
+          </div>
+
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${role}</p>
+              <p class="card-text"><small class="text-body-secondary">${email}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="./${img}" class="img-fluid rounded-start" alt="...">
+          </div>
+
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${role}</p>
+              <p class="card-text"><small class="text-body-secondary">${email}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="./${img}" class="img-fluid rounded-start" alt="...">
+          </div>
+
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${role}</p>
+              <p class="card-text"><small class="text-body-secondary">${email}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+  `;
+};
+
+let name = "";
+let role = "";
+let email = "";
+let img = "";
+
+for (let i = 0; i < teamMembers.length; i++) {
+  const currentCard = teamMembers[i];
+  name = currentCard.name;
+  role = currentCard.role;
+  email = currentCard.email;
+  img = currentCard.img;
+
+  cardMembers(card);
+
+  // cardMembers(card);
+}
